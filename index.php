d2902899894c1dfed4238f0dc81dc5fe558ce8dd
idevosmax <umartins82@gmail.com>
2019-07-09 23:36:42 +0100

    Changed few configs

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
 require "./Kernel.php";
 boot();
-// require "./src/Maxi/Request.php";
-// echo "<pre>";
-// print_r($_SERVER);
-// echo "</pre>";
--- routes.php
+++ routes.php
@@ -2,6 +2,19 @@
 use Maxi\Router;
 use Maxi\Request;
 
+
+Router::get("/", function ($req) {
+    return " 
+    <html>  
+    <head>
+    <title> Maxi's routing system home page </title>
+    </head>
+    <body>
+    </body>
+    </html>
+    
+    ";
+});
 Router::get("/home/:id/:name", function ($req, $params) {
     // echo $req->get('name');
     echo "<pre>";
@@ -10,8 +23,7 @@ Router::get("/home/:id/:name", function ($req, $params) {
 
 Router::post("/home", function ($req) {
     echo $req->get('name');
-    // echo "<pre>";
-    // print_r($req->postAll());
+    
 });
 Router::get("/login", function ($req) {
     echo $req->get('name');
@@ -23,33 +35,15 @@ Router::get("/login/:id", function ($req) {
 
 Router::get("/register", function ($req) {
   //  echo $req->get('name');
-  $res->title ="";
-  $res->type = "";
   
+
   return " 
   <html>  
   <head>
   <title> Maxi's routing system Sign up Page </title>
   </head>
   <body>
-
   </body>
-
   </html>
-  
   ";
-});
-Router::get("/", function ($req) {
-    return " 
-    <html>  
-    <head>
-    <title> Maxi's routing system home page </title>
-    </head>
-    <body>
-
-    </body>
-
-    </html>
-    
-    ";
 });
